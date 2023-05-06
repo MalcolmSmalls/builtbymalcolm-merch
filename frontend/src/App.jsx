192e9028eb7e3c3d57de234f7876c9820670f841
MalcolmSmalls <smallsbeats@gmail.com>
2023-05-06 11:47:03 -0700

    Fix hero and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import Hero from './Components/Hero'
+import ProductsScreen from './Screens/ProductsScreen'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <Router>
       <Hero />
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<ProductsScreen />} />
       </Routes>
     </Router>
   )
